3f5b7dec7797e364fd0bdb7fc29a623f4fb79127
Ярослав Максимов <sharveywilliams@gmail.com>
2019-08-02 15:15:19 +0300

    added project

---
+++ server.js
@@ -0,0 +1,14 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({port: 3000});
+
+server.on('connection', ws => {
+    ws.on('message', message => {
+        server.clients.forEach(client => {
+            if(client.readyState === WebSocket.OPEN){
+                client.send(message);
+            }
+        })
+    });
+    ws.send('Welcome to the chat');
+});
